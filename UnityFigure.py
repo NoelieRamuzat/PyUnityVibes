8279326f836d787dc0156943c171ec73390dae37
Noëlie Ramuzat <noelie.ramuzat@ensta-bretagne.org>
2018-02-06 14:23:30 +0100

    Fixing callbacks

--- UnityFigure.py
+++ UnityFigure.py
@@ -1,7 +1,6 @@
 from Network.TCPClient import TCPClient
 from UI.UnityObject import UnityObject
 from Network.Callback import Callback
-from UI.Color import Color
 import json
 
 class UnityFigure(object):
@@ -19,32 +18,30 @@ class UnityFigure(object):
     OBJECT_3D_CYLINDER = 4
     OBJECT_3D_BOAT = 5
     OBJECT_3D_SUBMARINE = 6
-    OBJECT_3D_DEATHSTAR = 7
+    OBJECT_3D_GALLEON = 7
 
-    COLOR_RED = Color(255, 0, 0, 255)
-    COLOR_GREEN = Color(0, 255, 0, 255)
-    COLOR_BLUE = Color(0, 0, 255, 255)
-    COLOR_YELLOW = Color(255, 255, 0, 255)
-    COLOR_CYAN = Color(0, 255, 255, 255)
-    COLOR_PINK = Color(255, 0, 255, 255)
-    COLOR_ORANGE = Color(255, 100, 0, 255)
-    COLOR_PURPLE = Color(67, 13, 83, 255)
-    COLOR_BLACK = Color(0, 0, 0, 255)
-    COLOR_WHITE = Color(255, 255, 255, 255)
+    COLOR_RED = "RED"
+    COLOR_GREEN = "GREEN"
+    COLOR_BLUE = "BLUE"
+    COLOR_YELLOW = "YELLOW"
+    COLOR_CYAN = "CYAN"
+    COLOR_PINK = "PINK"
+    COLOR_ORANGE = "ORANGE"
+    COLOR_PURPLE = "PURPLE"
+    COLOR_BLACK = "BLACK"
+    COLOR_WHITE = "WHITE"
 
-    def __init__(self, typeFig, background='plane', color='green', dimX=50, dimY=50, dimZ=50):
+    def __init__(self, typeFig, dimX=50, dimY=50, dimZ=50):
         self.tcpClient = TCPClient(self.onMessageReceived)  # port
         self.tcpClient.connect()  ##192.168.1.25
         self.msgId = 0
         self.objId = 0
         self.msgCallbacks = {}
-        self.init(typeFig, background, color, dimX, dimY, dimZ)
+        self.init(typeFig, dimX, dimY, dimZ)
 
-    def init(self, typeFig, background, color, dimX, dimY, dimZ):
+    def init(self, typeFig, dimX, dimY, dimZ):
         fig = {
             'typeFig': typeFig,
-            'background': background,
-            'color': color,
             'dimX': dimX,
             'dimY': dimY,
             'dimZ': dimZ
@@ -75,8 +72,8 @@ class UnityFigure(object):
         return newObj
 
     def onCreated(self, id, obj):
-        print("Objet créé")
         obj.id = id
+        print(obj.type + obj.id + "created")
 
     def sendAction(self, action, content, callback = None):
         obj = {
@@ -91,26 +88,27 @@ class UnityFigure(object):
         self.msgId += 1
 
     def onMessageReceived(self, message):
-        self.tcpClient.messageID = message[0]
-        try:
-            message = message[1].decode("ascii").replace("\\", "")
-            print("Received: ", message)
-            msg = json.loads(message)
 
-            idObject = msg['content']['id']
-            msgId = msg['msgId']
-            msgContent = msg['content']
-            msgAction = msg['action']
-            if msgId in self.msgCallbacks.keys():
-                callback = self.msgCallbacks[msgId]
-                if msgAction == 'Get':
-                    callback.function(msgContent)
-                elif msgAction == 'Create':
-                    callback.function(idObject, callback.object)
-                else:
-                    callback.function()
-        except Exception as e:
-            pass
+        message = message.decode("ascii").replace("\\", "").replace("(Object3D)", "")
+        print("Received: ", message)
+        msg = json.loads(message)
+        print(msg)
+        #try:
+
+        self.tcpClient.messageID = msg['msgId']
+        msgId = self.tcpClient.messageID
+        idObject = msg['objId']
+        msgAction = msg['type']
+        if msgId in self.msgCallbacks.keys():
+            callback = self.msgCallbacks[msgId]
+            if msgAction == 'Get':
+                callback.function(msg['objContent'])
+            elif msgAction == 'Create':
+                callback.function(idObject, callback.object)
+            else:
+                callback.function()
+        #except Exception as e:
+         #   pass
 
 
 
